85ee5e291f43b664312f4624e7d65289ed6d0d8f
Enrico Joerns <e.joerns@tu-bs.de>
2013-12-17 17:49:07 +0100

    [ævr-logger] Added gyroscope to demo

--- avr-logger/demo.c
+++ avr-logger/demo.c
@@ -3,6 +3,7 @@
 #include <stdio.h> /* For printf() */
 #include "rs232.h"
 #include "acc-sensor.h"
+#include "gyro-sensor.h"
 
 // allows to send data via rs232
 void my_send(char dat) {
@@ -23,11 +24,20 @@ PROCESS_THREAD(hello_world_process, ev, data)
   // get pointer to sensor
   static const struct sensors_sensor *acc_sensor;
   acc_sensor = sensors_find("Acc");
+  static const struct sensors_sensor *gyro_sensor;
+  gyro_sensor = sensors_find("Gyro");
 
+  uint8_t status;
   // activate and check status
-  uint8_t status = SENSORS_ACTIVATE(*acc_sensor);
+  status = SENSORS_ACTIVATE(*acc_sensor);
   if (status == 0) {
     printf("Error: Failed to init accelerometer, aborting...\n");
+    PROCESS_EXIT();
+  }
+  // activate and check status
+  status = SENSORS_ACTIVATE(*gyro_sensor);
+  if (status == 0) {
+    printf("Error: Failed to init gyroscope, aborting...\n");
     PROCESS_EXIT();
   }
 
@@ -41,20 +51,25 @@ PROCESS_THREAD(hello_world_process, ev, data)
 
   asdl_start_logger(&my_logger);
 
+  int32_t val0[3] = {123, 234, 222};
+  int32_t val1[2] = {500, 500};
+
   while(1)
   {
-    int32_t val0[3] = {123, 234, 222};
     val0[0] = acc_sensor->value(ACC_X);
     val0[1] = acc_sensor->value(ACC_Y);
     val0[2] = acc_sensor->value(ACC_Z);
     //printf("x: %ld, y: %ld, z: %ld\n", val0[0], val0[1], val0[2]);
     asdl_log(&my_logger, 0, val0);
-    int32_t val1[2] = {500, 500};
+
+    val1[0] = gyro_sensor->value(GYRO_X);
+    val1[1] = gyro_sensor->value(GYRO_Y);
     asdl_log(&my_logger, 1, &val1);
+
     int16_t val2 = 42;
     asdl_log(&my_logger, 2, &val2);
 
-    etimer_set(&et, CLOCK_SECOND / 32);
+    etimer_set(&et, CLOCK_SECOND / 64);
 
     PROCESS_YIELD();
   }
